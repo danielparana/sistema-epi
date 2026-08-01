fabb5033b4c628d190ddc5baee60cab0a1a09e3f
Fabricio Torri <fabricio_torri@estudante.sesisenai.org.br>
2026-08-01 00:08:51 -0300

    Configura API para produção no Render

--- frontend/assets/js/api.js
+++ frontend/assets/js/api.js
@@ -1,3 +1,6 @@
-const API_URL = !window.location.hostname || window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
-  ? 'http://localhost:3000'
-  : 'https://expert-invention-jrj7v4r9pgwv39qp-3000.app.github.dev';
+const API_URL =
+              !window.location.hostname ||
+               window.location.hostname === 'localhost' ||
+               window.location.hostname === '127.0.0.1'
+    ? 'http://localhost:3000'
+    : 'https://sistema-epi-backend.onrender.com';
