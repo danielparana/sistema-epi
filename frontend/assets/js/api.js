5ac867122898c6d8e89d7aad0b50e5cefafb2efa
Fabricio Torri <fabricio_torri@estudante.sesisenai.org.br>
2026-08-01 00:23:42 -0300

    Corrige API_URL global

--- frontend/assets/js/api.js
+++ frontend/assets/js/api.js
@@ -1,6 +1,6 @@
-const API_URL =
-              !window.location.hostname ||
-               window.location.hostname === 'localhost' ||
-               window.location.hostname === '127.0.0.1'
+window.API_URL =
+  !window.location.hostname ||
+  window.location.hostname === 'localhost' ||
+  window.location.hostname === '127.0.0.1'
     ? 'http://localhost:3000'
     : 'https://sistema-epi-backend.onrender.com';
